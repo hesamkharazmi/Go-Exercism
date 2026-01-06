9a3181d5574e0309c5b18237ac862346a19e6fff
hesam kharazmi <hesamkharazmiii@gmail.com>
2026-01-06 14:55:57 +0330

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -3,8 +3,8 @@ package reverse
 func Reverse(input string) string {
 	r := []rune(input)
 
-    for i , j :=0 , len(r)-1 ; i<j ; i , j = i+1 , j-1{
-        r[i] , r[j] = r[j] , r[i]
-    }
-    return string(r)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
